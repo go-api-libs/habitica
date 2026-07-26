465fbc57e464bfa6225da1db9c903ad7de3cc679
Mark Rosemaker <48681726+MarkRosemaker@users.noreply.github.com>
2026-07-26 15:23:21 +0200

    fix interaction

--- pkg/habitica/client.gen_test.go
+++ pkg/habitica/client.gen_test.go
@@ -512,9 +512,9 @@ func TestClient_Interactions(t *testing.T) {
 		t.Fatalf("ScoreTask: got: %T, want: *Error", err)
 	}
 
-	// if _, err := c.ScoreTask(ctx, uuid.MustParse("488a92c0-164c-445a-be31-1bcb1b04ab04"), "up", ScoreTaskParams{
-	// 	XAPIUser: uuid.MustParse("8027d396-e2bb-4389-b002-782025424e75"),
-	// }); err != nil {
-	// 	t.Fatalf("ScoreTask: %v", err)
-	// }
+	if _, err := c.ScoreTask(ctx, uuid.MustParse("488a92c0-164c-445a-be31-1bcb1b04ab04"), "up", ScoreTaskParams{
+		XAPIUser: uuid.MustParse("b0413351-405f-416f-8787-947ec1c85199"),
+	}); err != nil {
+		t.Fatalf("ScoreTask: %v", err)
+	}
 }
